a02eb2ec072a8f7a4afa3edec54446a65cb2538c
Jeremy Blair <jeremy@redarchlabs.com>
2025-05-22 14:08:58 -0600

    Added thread safety and support for service in get_logger

--- redarch_logging_client/log_manager.py
+++ redarch_logging_client/log_manager.py
@@ -2,19 +2,26 @@
 
 from .logger import Logger
 import os
+import threading
+
 class LoggerManager:
     def __init__(self):
         self._loggers = {}
+        self._lock = threading.Lock()
         self._default_level = os.getenv("RARCH_LOGGING_DEFAULT_LEVEL", "DEBUG").upper()
 
-    def get_logger(self, name):
-        if name not in self._loggers:
-            self._loggers[name] = Logger(service=name, level=self._default_level)
-        return self._loggers[name]
+    def get_logger(self, service: str, logger_name: str = None):
+        logger_name = logger_name or service
+        with self._lock:
+            if logger_name not in self._loggers:
+                self._loggers[logger_name] = Logger(service=service, level=self._default_level)
+            return self._loggers[logger_name]
 
-    def set_log_level(self, name, level):
-        logger = self.get_logger(name)
-        logger.level = level.upper()
+    def set_log_level(self, service: str, logger_name: str = None, level: str = "DEBUG"):
+        logger_name = logger_name or service
+        with self._lock:
+            logger = self.get_logger(service, logger_name)
+            logger.level = level.upper()
 
 # Singleton instance
 logger_manager = LoggerManager()
